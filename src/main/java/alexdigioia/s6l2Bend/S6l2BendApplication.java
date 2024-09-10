423072ec36fc2ab18a60976932333cbd846c2930
AlexDiGioia <digioia.alex2001@gmail.com>
2024-09-10 14:41:02 +0200

    Initial commit

---
+++ src/main/java/alexdigioia/s6l2Bend/S6l2BendApplication.java
@@ -0,0 +1,13 @@
+package alexdigioia.s6l2Bend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S6l2BendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S6l2BendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alexdigioia/s6l2Bend/S6l2BendApplicationTests.java
@@ -0,0 +1,13 @@
+package alexdigioia.s6l2Bend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S6l2BendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
